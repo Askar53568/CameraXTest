f2a69fb985d3016a7d741d14a9c7b8c53a756a85
Askar53568 <diana.suankulova@gmail.com>
2022-03-29 20:01:29 +0100

    Database push+retreive done

--- app/src/main/java/com/example/cameraxtest/MainActivity.kt
+++ app/src/main/java/com/example/cameraxtest/MainActivity.kt
@@ -4,14 +4,31 @@ import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.ListView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.cameraxtest.databinding.ActivityMainBinding
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
 import com.google.firebase.database.ktx.getValue
+import java.util.*
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    //Maps
+    private lateinit var mMap: GoogleMap
+    private lateinit var listView: ListView
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var POIs : MutableList<PoI>
 
     private lateinit var auth: FirebaseAuth
 
@@ -24,7 +41,12 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        firebaseDatabase = FirebaseDatabase.getInstance("https://map-login-57509-default-rtdb.europe-west1.firebasedatabase.app/")
+
+        executeMap()
+
         auth = FirebaseAuth.getInstance()
 
         if(auth.currentUser == null){
@@ -49,26 +71,37 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, UpdatePassword::class.java)
             startActivity(intent)
         }
-
         executeDatabase()
+        writeNewPOI("Koktobe", LatLng(43.242013, 76.959379), "description")
+        writeNewPOI("Swansea Uni", LatLng(51.619174, -3.880502), "uni")
+    }
+    private fun executeMap(){
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
     }
 
-    private fun executeDatabase(){
-        firebaseDatabase = FirebaseDatabase.getInstance("https://map-login-57509-default-rtdb.europe-west1.firebasedatabase.app/")
+    private fun executeDatabase() {
         dbReference = firebaseDatabase.reference
-        dbReference = dbReference.child("qq")
+        dbReference = dbReference.child("users")
         val myRef = firebaseDatabase.getReference("message")
-        val userRef = firebaseDatabase.getReference("users")
+        val favourites = listOf("Swansea", "Neath")
+        writeNewUserAuth(favourites)
+    }
 
-        writeNewUser("newUser1", "Swansea Bay")
+    private fun writeNewPOI(name: String, location : LatLng, description: String){
+        dbReference = firebaseDatabase.reference
+        val uuid = UUID.randomUUID().toString()
+        val poI = PoI(uuid, name, location, description)
+        val childUpdates = hashMapOf<String, Any>(
+            "/POIs/$uuid" to poI
+        )
+        dbReference.updateChildren(childUpdates)
 
-
-
-
-        myRef.setValue("Hello, World!")
-
+    }
+        /*
         userRef.addValueEventListener(object : ValueEventListener {
-            override fun onDataChange(dataSnapshot: DataSnapshot) {
+             override fun onValueChange(dataSnapshot: DataSnapshot) {
                 // This method is called once with the initial value and again
                 // whenever data at this location is updated.
                 val map = dataSnapshot.value as Map<String, Any>?
@@ -78,6 +111,10 @@ class MainActivity : AppCompatActivity() {
                 Log.d(ContentValues.TAG, "Value is: ${map}")
             }
 
+            override fun onDataChange(snapshot: DataSnapshot) {
+                TODO("Not yet implemented")
+            }
+
             override fun onCancelled(error: DatabaseError) {
                 // Failed to read value
                 Log.w(ContentValues.TAG, "Failed to read value.", error.toException())
@@ -85,12 +122,13 @@ class MainActivity : AppCompatActivity() {
         })
     }
 
+         */
+
+    /*
     private fun writeNewUser(username: String, favourite: String) {
-        // Create new post at /user-posts/$userid/$postid and at
-        // /posts/$postid simultaneously
         val key = dbReference.child("users").push().key
         if (key == null) {
-            Log.w(TAG, "Couldn't get push key for posts")
+            Log.w(TAG, "Couldn't get push key for users")
             return
         }
 
@@ -103,5 +141,145 @@ class MainActivity : AppCompatActivity() {
         )
 
         dbReference.updateChildren(childUpdates)
+    }
+
+     */
+
+    private fun writeNewUserAuth(favourites: List<String>) {
+        val user = FirebaseAuth.getInstance().currentUser!!
+        val uId = user.uid
+
+        val childUpdates = hashMapOf<String, Any>(
+            "$uId/username" to user.email!!,
+            "$uId/favourites" to favourites
+        )
+
+        dbReference.updateChildren(childUpdates)
+    }
+
+    private fun listenerPOI(){
+        dbReference = firebaseDatabase.reference
+        dbReference = dbReference.child("POIs")
+        val childEventListener = object : ChildEventListener {
+            override fun onChildAdded(dataSnapshot: DataSnapshot, previousChildName: String?) {
+                Log.d(TAG, "onChildAdded:" + dataSnapshot.key!!)
+
+                // A new comment has been added, add it to the displayed list
+                val PoI = dataSnapshot.getValue<PoI>()
+                Log.d(ContentValues.TAG, "Value is: ${PoI}")
+                // ...
+            }
+
+            override fun onChildChanged(dataSnapshot: DataSnapshot, previousChildName: String?) {
+                Log.d(TAG, "onChildChanged: ${dataSnapshot.key}")
+
+                // A comment has changed, use the key to determine if we are displaying this
+                // comment and if so displayed the changed comment.
+                val newComment = dataSnapshot.getValue<PoI>()
+                val commentKey = dataSnapshot.key
+
+                // ...
+            }
+
+            override fun onChildRemoved(dataSnapshot: DataSnapshot) {
+                Log.d(TAG, "onChildRemoved:" + dataSnapshot.key!!)
+
+                // A comment has changed, use the key to determine if we are displaying this
+                // comment and if so remove it.
+                val commentKey = dataSnapshot.key
+
+                // ...
+            }
+
+            override fun onChildMoved(dataSnapshot: DataSnapshot, previousChildName: String?) {
+                Log.d(TAG, "onChildMoved:" + dataSnapshot.key!!)
+
+                // A comment has changed position, use the key to determine if we are
+                // displaying this comment and if so move it.
+                val movedComment = dataSnapshot.getValue<PoI>()
+                val commentKey = dataSnapshot.key
+
+                // ...
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                Log.w(TAG, "postComments:onCancelled", databaseError.toException())
+                Toast.makeText(this@MainActivity, "Failed to load comments.",
+                    Toast.LENGTH_SHORT).show()
+            }
+        }
+        dbReference.addChildEventListener(childEventListener)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        dbReference = firebaseDatabase.reference
+        dbReference = dbReference.child("POIs")
+        val childEventListener = object : ChildEventListener {
+            override fun onChildAdded(dataSnapshot: DataSnapshot, previousChildName: String?) {
+                Log.d(TAG, "onChildAdded:" + dataSnapshot.key!!)
+                val poi = PoI(
+                    dataSnapshot.key!!,
+                    dataSnapshot.child("name").getValue<String>()!!,
+                    LatLng(
+                        dataSnapshot.child("location/latitude").getValue<Double>()!!,
+                        dataSnapshot.child("location/longitude").getValue<Double>()!!
+                    ),
+                    dataSnapshot.child("description").getValue<String>()!!
+                )
+
+                mMap.addMarker(MarkerOptions().position(poi.location).title(poi.name))
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(poi.location,14f))
+                // A new comment has been added, add it to the displayed list
+            }
+
+            override fun onChildChanged(dataSnapshot: DataSnapshot, previousChildName: String?) {
+                Log.d(TAG, "onChildChanged: ${dataSnapshot.key}")
+
+                val children = dataSnapshot.children
+                val poi = PoI(
+                    dataSnapshot.key!!,
+                    dataSnapshot.child("name").getValue<String>()!!,
+                    LatLng(
+                        dataSnapshot.child("location/latitude").getValue<Double>()!!,
+                        dataSnapshot.child("location/longitude").getValue<Double>()!!
+                    ),
+                    dataSnapshot.child("description").getValue<String>()!!
+                )
+
+                mMap.addMarker(MarkerOptions().position(poi.location).title(poi.name))
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(poi.location,14f))
+
+                // ...
+            }
+
+            override fun onChildRemoved(dataSnapshot: DataSnapshot) {
+                Log.d(TAG, "onChildRemoved:" + dataSnapshot.key!!)
+
+                // A comment has changed, use the key to determine if we are displaying this
+                // comment and if so remove it.
+
+                // ...
+            }
+
+            override fun onChildMoved(dataSnapshot: DataSnapshot, previousChildName: String?) {
+                Log.d(TAG, "onChildMoved:" + dataSnapshot.key!!)
+
+                // A comment has changed position, use the key to determine if we are
+                // displaying this comment and if so move it.
+                // ...
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                Log.w(TAG, "postComments:onCancelled", databaseError.toException())
+                Toast.makeText(this@MainActivity, "Failed to load comments.",
+                    Toast.LENGTH_SHORT).show()
+            }
+        }
+        dbReference.addChildEventListener(childEventListener)
+//        val gymn56 = LatLng(43.247640, 76.948282)
+//        mMap.addMarker(MarkerOptions().position(gymn56).title("Marker in Gymnasium 56"))
+//
+//        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(gymn56,14f))
     }
 }

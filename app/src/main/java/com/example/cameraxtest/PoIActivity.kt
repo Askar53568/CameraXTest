f31945a22270ff99317557657979dceb47da7542
Askar53568 <diana.suankulova@gmail.com>
2022-03-31 09:17:32 +0100

    added image upload for individual POI activity

--- app/src/main/java/com/example/cameraxtest/PoIActivity.kt
+++ app/src/main/java/com/example/cameraxtest/PoIActivity.kt
@@ -1,12 +1,19 @@
 package com.example.cameraxtest
 
+import android.Manifest
+import android.app.Activity
 import android.content.ContentValues.TAG
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.example.cameraxtest.databinding.ActivityMainBinding
 import com.google.android.gms.maps.model.LatLng
 import com.google.firebase.database.*
 import com.google.firebase.database.ktx.getValue
@@ -14,15 +21,26 @@ import com.google.firebase.database.ktx.getValue
 class PoIActivity : AppCompatActivity() {
     //Create a database reference
     private lateinit var dbReference: DatabaseReference
+
     //Connect to the database
     private lateinit var firebaseDatabase: FirebaseDatabase
+
     //Textview for the details of the POI
-    private lateinit var detailsTv : TextView
+    private lateinit var detailsTv: TextView
+
+    //ImageView for the POI image
+    private lateinit var imagePOI: ImageView
+
+    //Camera and Gallery buttons
+    private lateinit var galleryButton: Button
+    private lateinit var cameraButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_poi)
         //connect to the database stored at the URL
-        firebaseDatabase = FirebaseDatabase.getInstance("https://map-login-57509-default-rtdb.europe-west1.firebasedatabase.app/")
+        firebaseDatabase =
+            FirebaseDatabase.getInstance("https://map-login-57509-default-rtdb.europe-west1.firebasedatabase.app/")
         //Get intent passed from the MainActivity.onMarkerClick
         var intent = getIntent()
         //Get the extra from the intent, which is a UUID of the POI
@@ -30,11 +48,33 @@ class PoIActivity : AppCompatActivity() {
 
         //Connect the TextView
         detailsTv = findViewById(R.id.details)
-
+        //Connect to the ImageView
+        imagePOI = findViewById(R.id.image)
+        //Get details of the POI and display them
         envokePOIListener(targetUUID)
+        //Set the listener for the imageview to be able to change the image
+        imagePOI.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+            {
+                if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED)
+                {
+                    //permission denied
+                    val permissions = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+                    //show popup to request runtime permission
+                    requestPermissions(permissions, IMAGE_PICK_CODE);
+                } else {
+                    //permission already granted
+                    pickImageFromGallery()
+                }
+            } else {
+                //system OS is < Marshmallow
+                pickImageFromGallery()
+            }
 
+        }
     }
-    private fun envokePOIListener(uuid: String){
+
+    private fun envokePOIListener(uuid: String) {
         //Actual database reference
         dbReference = firebaseDatabase.reference
         //Point at a specific node of the json tree to access the description of the specific POI of UUID ${uuid}
@@ -46,13 +86,54 @@ class PoIActivity : AppCompatActivity() {
                 //Set the description text as the textview
                 detailsTv.setText(description)
             }
+
             override fun onCancelled(databaseError: DatabaseError) {
-                Toast.makeText(this@PoIActivity, "Failed to load description", Toast.LENGTH_LONG).show()
+                Toast.makeText(this@PoIActivity, "Failed to load description", Toast.LENGTH_LONG)
+                    .show()
             }
         }
         //Attach the value listener to the database reference
         dbReference.addValueEventListener(POIListener)
 
 
+    }
+
+    companion object {
+        //image pick code
+        private val IMAGE_PICK_CODE = 1000
+
+        //Permission code
+        private val PERMISSION_CODE = 1001
+    }
+
+    private fun pickImageFromGallery() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, IMAGE_PICK_CODE)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode){
+            PERMISSION_CODE -> {
+                if (grantResults.size >0 && grantResults[0] ==
+                    PackageManager.PERMISSION_GRANTED){
+                    //permission from popup granted
+                    pickImageFromGallery()
+                }
+                else{
+                    //permission from popup denied
+                    Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    //handle result of picked image
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE){
+            imagePOI.setImageURI(data?.data)
+        }
     }
 }
